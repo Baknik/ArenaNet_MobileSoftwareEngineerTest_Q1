6cd75e35f4989e48679536b8175a281f895328e7
Baknik <rks3159@gmail.com>
2017-03-07 12:31:29 -0700

    Basic art and scrolling textures

---
+++ ArenaNet_MobileSoftwareEngineerTest_Q1/Assets/Scripts/World/ScrollingTexture.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollingTexture : MonoBehaviour {
+
+	[Header("Configuration")]
+	public string SortingLayerName;
+	public int SortingOrder;
+	public float ScrollSpeed = 1.0f;
+
+	private MeshRenderer myMeshRenderer;
+
+	void Awake()
+	{
+		this.myMeshRenderer = this.GetComponent<MeshRenderer>();
+	}
+
+	// Use this for initialization
+	void Start () {
+		this.myMeshRenderer.sortingLayerName = this.SortingLayerName;
+		this.myMeshRenderer.sortingOrder = this.SortingOrder;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.myMeshRenderer.material.mainTextureOffset =
+			new Vector2(
+				this.myMeshRenderer.material.mainTextureOffset.x + this.ScrollSpeed,
+				this.myMeshRenderer.material.mainTextureOffset.y
+			);
+	}
+}
